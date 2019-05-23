d968e0ad6efe992e097bab70582f7efa8c1fbc36
sauravgayawarAppliedIs <43874454+sauravgayawarAppliedIs@users.noreply.github.com>
2019-05-23 11:37:23 +0530

    Assignment View

--- AzureServiceCatalog.Web/app/blueprint/assign-blueprint.controller.js
+++ AzureServiceCatalog.Web/app/blueprint/assign-blueprint.controller.js
@@ -10,6 +10,18 @@
         var vm = this;
         vm.assignmentName = "";
         vm.blueprintVersion = null;
+
+        vm.lodash = _;
+        vm.parameters = [];
+        vm.resourceGroups = [];
+        vm.blueprintDefinitionVersion = initialData.name;
+        vm.blueprintName = initialData.properties.blueprintName;
+        vm.assignmentName = 'Assignment-' + vm.blueprintName;
+        vm.targetScope = initialData.properties.targetScope;
+
+
+
+
         activate();
 
         function activate() {
@@ -17,10 +29,41 @@
             var tempArr = vm.blueprintVersion.id.split('/');
             var subscriptionsIndex = tempArr.indexOf('subscriptions');
             vm.subscriptionId = tempArr[subscriptionsIndex + 1];
+
+
+            console.log('ResourceGroups');
+            console.log(initialData.properties.resourceGroups);
+
+
+            _.forIn(initialData.properties.parameters, function (value, key) {
+                vm.parameters.push({ name: key, info: value, value: value.defaultValue });
+            });
+
+            _.forIn(initialData.properties.resourceGroups, function (value, key) {
+                vm.resourceGroups.push({ name: key, info: value, value: value.name });
+            });
+
+            console.log('vm.resourceGroups');
+            console.log(vm.resourceGroups);
+
+            console.log('vm.parameters');
+            console.log(vm.parameters);
+        }
         }
 
         function assign() {
-            var blueprintAssignment = {  };
+            var blueprintAssignment = {
+                "identity": {
+                    "type": "SystemAssigned"
+                },
+                "location": "eastus",
+                "properties": {},
+                "resourceGroups": {}
+            };
+
+            _.forIn(vm.parameters, function (value, key) {
+                vm.resourceGroups.push({ name: key, info: value, value: value.name });
+            });
 
             ascApi.assignBlueprint(vm.subscriptionId, vm.assignmentName, blueprintAssignment).then(function (data) {
                 if (data.error) {
@@ -33,4 +76,4 @@
             });
         }
     }
-})();
+)();
