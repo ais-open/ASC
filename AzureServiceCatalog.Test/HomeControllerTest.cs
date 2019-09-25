539037be7f45edd6b210a1255fefd35072002bc3
sauravgayawarAppliedIs <43874454+sauravgayawarAppliedIs@users.noreply.github.com>
2019-09-25 07:07:50 +0530

    Added test project with a unittest to test

---
+++ AzureServiceCatalog.Test/HomeControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace AzureServiceCatalog.Test
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void CheckJsonErrorObjectIsNotNull()
+        {
+            //Arrange
+            string jsonError = "{\"error\":{\"code\":\"RoleDefinitionWithSameNameExists\",\"message\":\"A role definition cannot be updated with a name that already exists.\"}}";
+            dynamic errorObject = JObject.Parse(jsonError);
+
+            //Act
+            var errorJson = errorObject.error;
+
+            //Assert
+            Assert.IsNotNull(errorJson);
+        }
+    }
+}
---
+++ AzureServiceCatalog.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AzureServiceCatalog.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AzureServiceCatalog.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("29178cc8-f2c6-4bd4-8587-c972cda94d5d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
